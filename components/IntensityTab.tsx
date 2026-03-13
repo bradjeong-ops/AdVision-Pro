abff79b7b675709ef7a80b33aedfb4a535b3aed0
advision-pro <bradjeong@descente.co.kr>
2026-03-13 11:32:25 +0900

    Update IntensityTab.tsx

--- components/IntensityTab.tsx
+++ components/IntensityTab.tsx
@@ -306,6 +306,7 @@ const IntensityTab: React.FC<IntensityTabProps> = ({
             downloadImage={downloadImage}
             showTransfer
             onTransfer={onTransferToInput}
+            columns="grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
           />
         </div>
       </div>
